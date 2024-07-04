c36bcc988dbc886e552fddac806b5c6ff94fc75d
MUHAMMED HIJAS K <hijaz2688@gmail.com>
2024-07-04 16:08:27 +0400

    UI-final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeesComponent } from './employees/employees.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'DashBoard', component: DashboardComponent},
+    {path: 'employees', component: EmployeesComponent},
+];
